84f96d62dc3c75bb617a98f2024374cca6f01ee6
Lipsita <lipsita.987@gmail.com>
2020-04-13 11:53:44 +0530

    login class modified

--- src/test/java/com/qa/pages/Login.java
+++ src/test/java/com/qa/pages/Login.java
@@ -1,17 +1,20 @@
 package com.qa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {	
 	
 	public static void main(String[] args){
-		
 		System.setProperty("webdriver.chrome.driver", "F://Downloads//chromedriver801.exe");
 		WebDriver driver = new ChromeDriver();
 	    driver.get("https://www.google.com");
-		driver.quit();		
-		
+	    driver.manage().window().maximize();
+	   driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("TestAutomation");
+	   System.out.println("Entered text is searched");    
+		driver.quit();
+
 	}	
 
 }
